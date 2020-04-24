275fe969075d6f60937ca50b57f12d1610da0c71
Alexander Danylchenko <olexander.danilchenko@gmail.com>
2020-04-24 19:26:29 +0300

    Added clients controller

---
+++ src/main/java/ua/alexd/CarRentService/controller/ClientResource.java
@@ -0,0 +1,58 @@
+package ua.alexd.CarRentService.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ua.alexd.CarRentService.domain.Client;
+import ua.alexd.CarRentService.service.ClientService;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@CrossOrigin(origins = {"http://localhost:3000", "http://localhost:4200", "http://localhost:8081"})
+@RestController
+@RequestMapping("/client")
+public class ClientResource {
+    private final ClientService clientService;
+
+    public ClientResource(ClientService clientService) {
+        this.clientService = clientService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Client>> getClients() {
+        var clients = clientService.getAllClients();
+        return clients.isEmpty()
+                ? new ResponseEntity<>(HttpStatus.NOT_FOUND)
+                : new ResponseEntity<>(clients, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Client> getClientById(@PathVariable String id) {
+        var foundedClient = clientService.getClientById(id);
+        return foundedClient
+                .map(client -> new ResponseEntity<>(client, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping
+    public ResponseEntity<Client> addClient(@Valid @RequestBody Client newClient) {
+        return clientService.addNewClient(newClient)
+                ? new ResponseEntity<>(newClient, HttpStatus.CREATED)
+                : new ResponseEntity<>(HttpStatus.CONFLICT);
+    }
+
+    @PutMapping
+    public ResponseEntity<Client> updateClient(@Valid @RequestBody Client updClient) {
+        return clientService.updateClient(updClient)
+                ? new ResponseEntity<>(updClient, HttpStatus.OK)
+                : new ResponseEntity<>(HttpStatus.CONFLICT);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteClient(@PathVariable String id) {
+        return clientService.deleteClient(id)
+                ? new ResponseEntity<>(HttpStatus.NO_CONTENT)
+                : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+}
---
+++ src/main/java/ua/alexd/CarRentService/service/ClientService.java
@@ -0,0 +1,65 @@
+package ua.alexd.CarRentService.service;
+
+import org.apache.commons.lang3.math.NumberUtils;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.beans.BeanUtils;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Service;
+import ua.alexd.CarRentService.domain.Client;
+import ua.alexd.CarRentService.repository.ClientRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ClientService {
+    private final ClientRepository clientRepository;
+
+    public ClientService(ClientRepository clientRepository) {
+        this.clientRepository = clientRepository;
+    }
+
+    public List<Client> getAllClients() {
+        return (List<Client>) clientRepository.findAll();
+    }
+
+    public Optional<Client> getClientById(String id) {
+        try {
+            var longId = NumberUtils.createLong(id);
+            return clientRepository.findById(longId);
+        } catch (NumberFormatException e) {
+            return Optional.empty();
+        }
+    }
+
+    public boolean addNewClient(Client newClient) {
+        return saveClient(newClient);
+    }
+
+    public boolean updateClient(@NotNull Client updClient) {
+        var clientFromDB = getClientById(String.valueOf(updClient.getId()));
+        if (clientFromDB.isPresent()) {
+            BeanUtils.copyProperties(updClient, clientFromDB.get(), "id");
+            return saveClient(clientFromDB.get());
+        }
+        return false;
+    }
+
+    private boolean saveClient(Client saveClient) {
+        try {
+            clientRepository.save(saveClient);
+        } catch (IllegalArgumentException | DataIntegrityViolationException ignored) {
+            return false;
+        }
+        return true;
+    }
+
+    public boolean deleteClient(String id) {
+        var deletionClient = getClientById(id);
+        if (deletionClient.isPresent()) {
+            clientRepository.delete(deletionClient.get());
+            return true;
+        }
+        return false;
+    }
+}
